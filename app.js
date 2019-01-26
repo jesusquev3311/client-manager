26788ced4f8cc558dbec5bc421605de8ab8c1487
jesusquev3311 <jesusquev3311@gmail.com>
2019-01-25 22:49:34 -0500

    basic auth

--- app.js
+++ app.js
@@ -31,6 +31,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//user authentification
+function auth(req,res,next){
+  
+var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == 'admin' && pass == 'password') {
+      next(); // authorized
+  } else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      next(err);
+  }
+}
+
+//require Auth
+app.use(auth);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
