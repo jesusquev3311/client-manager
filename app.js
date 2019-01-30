12912250e36ea570b3b1f54bfba6731af99ca323
jesus quevedo <jesus@gokapital.com>
2019-01-30 14:21:38 -0500

    mongo pruebas

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 //database URL
 //local
-const url = 'mongodb://127.0.0.1:27017/capstone';
+const url = 'mongodb://root:codigo=3311@127.0.0.1:27017/capstone';
 
 
 //connection to Mongo Database
